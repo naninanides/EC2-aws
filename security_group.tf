3f3f561309819c6c6d1b1291746e07843599cce3
naninanides <baihaqisalmon@gmail.com>
2025-07-23 10:58:50 +0700

    fix security group name

--- security_group.tf
+++ security_group.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "allow_all_ci" {
-  name        = "allow_all"
-  description = "Allow all inbound and outbound traffic"
+  name        = "allow_all_ci"
+  description = "Allow all in gitlab ci inbound and outbound traffic"
   vpc_id      = var.vpc_id
 
   ingress {
