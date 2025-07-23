c2167ac8bc7e5bea7e02821d78807abf4f2bd89f
naninanides <baihaqisalmon@gmail.com>
2025-07-23 09:51:18 +0700

    add pipeline and add testing instance

--- provider.tf
+++ provider.tf
@@ -9,7 +9,7 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "${AWS_DEFAULT_REGION}"
+  region = "ap-southeast-3"
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
