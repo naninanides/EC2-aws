3f4b01a8de4bd8e6a21513ccdbcca05c74eafd0e
naninanides <baihaqisalmon@gmail.com>
2025-08-20 15:37:19 +0700

    update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket         = "terraform-baihaqi"
     key            = "tfstate/terraform.tfstate"
     region         = "ap-southeast-3"
-    dynamodb_table = "terraform-locks"   # untuk locking
     encrypt        = true
   }
 }
